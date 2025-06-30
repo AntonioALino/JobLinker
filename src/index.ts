4ec06f7b60151c886e34c89bac356493a7b43fc6
AntonioALino <antonioaplino@gmail.com>
2025-06-30 14:37:56 -0300

    :tada: Starting project setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`[server]: API running at http://localhost:${port}`);
+});
